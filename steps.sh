889ec2841a094692741434f49a7e75c401173892
Vaibhav Sagar <vaibhav.sagar@obsidian.systems>
2019-06-20 11:35:17 -0400

    steps.sh: init

---
+++ steps.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env nix-shell
+#! nix-shell -i bash
+#! nix-shell -p cabal-install git haskellPackages.ghcid nix
+
+set -euxo pipefail
+
+git clone --recursive https://gitlab.haskell.org/ghc/ghc/
+cd ghc
+git clone https://github.com/alpmestan/ghc.nix
+cabal update
+nix-shell ghc.nix --run './boot && ./configure && ghcid --run=":q"' 2>&1 | tee output.txt
+if [ "$(tail -n2 output.txt | head -n1)" == "Leaving GHCi." ] ; then
+  exit 0
+else
+  exit 1
+fi
