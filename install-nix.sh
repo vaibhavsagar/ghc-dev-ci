ac94ddd8093b77fe1b3e1ea94c6ee316f27c5ccc
Vaibhav Sagar <vaibhav.sagar@obsidian.systems>
2019-06-21 18:09:56 -0400

    .circleci/config.yml: update

--- install-nix.sh
+++ install-nix.sh
@@ -3,6 +3,6 @@
 export NIX_CURL_FLAGS=-sS
 wget --retry-connrefused --waitretry=1 -O /tmp/nix-install https://nixos.org/releases/nix/nix-2.2.2/install
 yes | sh /tmp/nix-install
-source /home/ubuntu/.nix-profile/etc/profile.d/nix.sh
+source /home/circleci/.nix-profile/etc/profile.d/nix.sh
 nix --version
 nix-instantiate --eval -E 'with import <nixpkgs> {}; lib.version or lib.nixpkgsVersion'
