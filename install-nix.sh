5756f2c16f6fd767fe5d8f21ff2627ca4e6b602e
Vaibhav Sagar <vaibhav.sagar@obsidian.systems>
2019-06-21 18:01:20 -0400

    install-nix.sh: add shebang line

---
+++ install-nix.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export NIX_CURL_FLAGS=-sS
+wget --retry-connrefused --waitretry=1 -O /tmp/nix-install https://nixos.org/releases/nix/nix-2.2.2/install
+yes | sh /tmp/nix-install
+source /home/ubuntu/.nix-profile/etc/profile.d/nix.sh
+nix --version
+nix-instantiate --eval -E 'with import <nixpkgs> {}; lib.version or lib.nixpkgsVersion'
