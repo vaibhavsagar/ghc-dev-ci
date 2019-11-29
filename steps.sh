0908f297e0dd549da34994935ef99f1fb8160af7
Vaibhav Sagar <vaibhavsagar@gmail.com>
2019-11-28 22:56:47 -0500

    steps.sh: don't exit on expected non-zero exit code

--- steps.sh
+++ steps.sh
@@ -8,5 +8,5 @@ git clone --recursive https://gitlab.haskell.org/ghc/ghc/
 cd ghc
 git clone https://github.com/alpmestan/ghc.nix
 cabal update
-nix-shell ghc.nix --run './boot && ./configure && ghcid --run=":q" -o output.txt'
+nix-shell ghc.nix --run './boot && ./configure && ghcid --run=":q" -o output.txt' || true
 tail -n2 output.txt | head -n1 | grep 'All good'
