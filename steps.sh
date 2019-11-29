9e59179ab166eac2a8795631a725c4fdcddb403e
Vaibhav Sagar <vaibhavsagar@gmail.com>
2019-11-28 22:37:44 -0500

    steps.sh: output from ghcid directly

--- steps.sh
+++ steps.sh
@@ -8,9 +8,5 @@ git clone --recursive https://gitlab.haskell.org/ghc/ghc/
 cd ghc
 git clone https://github.com/alpmestan/ghc.nix
 cabal update
-nix-shell ghc.nix --run './boot && ./configure && ghcid --run=":q"' 2>&1 | tee output.txt
-if [ "$(tail -n2 output.txt | head -n1)" == "Leaving GHCi." ] ; then
-  exit 0
-else
-  exit 1
-fi
+nix-shell ghc.nix --run './boot && ./configure && ghcid --run=":q" -o output.txt'
+tail -n2 output.txt | head -n1 | grep 'All good'
